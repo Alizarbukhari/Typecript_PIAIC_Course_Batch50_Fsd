bbecfbaffd06620c20ae724fcab68e494996d622
Alizarbukhari <Alizar.fsd@gmail.com>
2023-08-21 22:39:36 +0500

    class05

---
+++ Class05/index.js
@@ -0,0 +1,99 @@
+"use strict";
+// let num:number = 1;
+// while (num <=10) {
+//     var result:number = num * 7
+//     console.log("7 *",num ,"=",result);
+//     num = num +1
+///////////                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        }
+// let index:number[]= [1,2,3,4,5,]
+// index.pop()
+// console.log(index);
+// var i:number =1 ;
+// var result:number = 2 + 4+ i++;
+// var result:number = 2 + 4+ ++i;
+// //console.log(i);
+// console.log(result);
+////pre fix pehly azafa hoga fr calculate kro
+////post fix pehly calclate kro fr azafa kro
+// //for
+// var i:number = 15;
+// do{
+// console.log("naveed");
+// i++;
+// }while (i <= 30) 
+//write a ts program that calculate sum of even number between 1 to 100; 
+// let sum:number = 0;
+// for(let i = 1; i<=100; i++){
+// //console.log(i);
+// if(i % 2==0){
+// sum = sum +1 
+//     //console.log("even");
+// }
+// }
+//  console.log("sum",sum);
+// let sum:number = 1;
+// let list:number [] = []
+// for (let i = 1; i <=100; i++) {
+//     if (i%2 ==0) {
+//         sum = sum +i
+// console.log("i",i);
+// list.push(i)
+//     }
+// }
+// console.log("sum",sum,list);
+// function sum1(array:number[]){
+//     let i:number = 0;
+//     let sum2:number =0;
+//     while (i <array.length) {
+//      console.log(array[i]);
+//      sum2 = sum2 + array[i]
+//      i++   
+//     }
+//     return sum2;
+// }
+//  console.log("result of function",sum1([1,2,3,4,5]));
+// function factorial(num1:number){
+//     if(num1>0){}
+//     let result:number = 1;
+// return;
+// }
+// console.log("Number is Zero or negative");
+// factorial (5)
+// //num1 * num1+ - 1 + num1 * num1 -2 + 0;
+var index1 = 1;
+var evenCount = 0;
+//console.log("even number");
+// while (evenCount < 10) {
+//     if (index1 % 2 == 0) {
+//         console.log(index1);
+//         evenCount++
+//     }
+//     index1++;
+// }
+// var index2:number = 1
+// var oddCount:number = 0;
+// console.log("odd numbeer");
+// while (oddCount<10) {
+//     if (index1%3 == 0) {
+//      console.log(index1);
+//         oddCount++
+//     }
+//     index1++
+// }
+// let n:number = 0;
+// while (n<=25) {
+//     console.log(n);
+//     n = n + 1
+// }
+// function factorial(number1:number) {
+//     var factorialCal:number = 1;
+//     while (number1 != 0) {
+//  factorialCal * number1;
+//  number1 --;       
+//     }
+//     return factorialCal
+// }
+// var result = factorial(5)
+// console.log("factorial of number ",result);
+let array = [-5, 2, 3, 4, 5,];
+console.log(`Array befor removing${array}`);
