fb15dfb22a24cffc31ae6806d6b3658e5a3dfac8
Alizarbukhari <Alizar.fsd@gmail.com>
2023-08-21 22:53:03 +0500

    class04

---
+++ Class 04/index.js
@@ -0,0 +1,139 @@
+"use strict";
+////Arry
+// let name1:string = "Abu Huraira";
+// let name2:string = "Naveed Sarwer";
+// let names: string[] = ["Abu HUraira", "Naveer Sarwer","Bilal","kashsif"];
+// console.log(names)[2];
+// let rollNumber: number[] = [10, 20, 30, 40];
+// let sum = 
+// console.log();
+// console.error();
+// console.warn();
+// let names:string[] = ["alizar","bilal",];
+// names.push("ali");
+// names.pop();
+// console.log("afer add",names);
+// let names: string []= ["abu huraira", "naveed Sarwar","Ali"];
+// names.push("Iqbal");
+// names.pop();
+// names.push("waseem")
+// console.log(names);
+// let names: string[] = ["abu Huraira","Wasee,","iqbal"];
+// names.unshift();
+// console.log(names);
+// let names1: string[] = ["abu Huraira","Wasee,","iqbal"];
+// names.shift();
+// console.log(names);
+// let names1: string[] = ["abu Huraira","Wasee,","iqbal"];
+// names1.splice(1,0,"nasir","Ahmad","jawad")
+// console.log(names1);
+// let names1: string[] = ["abu Huraira","Wasee,","iqbal"];
+// names1.splice(2,1,"nasir","Ahmad","jawad")
+// console.log(names1);
+// let names1: string[] = ["abu Huraira","Wasee,","iqbal"];
+// console.log(names1.length);
+// // ////lenght -1 = last index
+// let todo:string[] = ["good Morning ","lunch","dinner"];
+// todo.splice(1,1,"nashta");
+// console.log(todo);
+// let todo1:string[] = ["good Morning ","lunch","dinner","ali","kashif"];
+// if ( )todo1.splice(0,1,);
+// console.log(todo1);
+// //Array  save cells Consectively not randomlly. start from 0,1,2,3,4,5......;
+// var rolNo:number[] = [2220,2221,2222,2223,0,2225,]
+// rolNo [0] = 2219;
+// rolNo.push(2226);
+// rolNo [4]=2224;
+// rolNo.unshift(2218)
+// console.log(rolNo);
+// var index:number = 1;
+// while(index<= 10){
+//     console.log("before loop");
+//     console.log("print")
+//     index = index +1
+// // }
+// console.log(("after loop"));
+// console.log("print");
+// console.log("print");
+// console.log("print");
+// console.log("print");
+// console.log("print");
+// console.log("print");
+// console.log("print");
+// console.log("print");
+// // console.log("print");
+// var index:number = 1;
+// while(index<=20){
+//     var result:number = index *3
+//     console.log("3 *",index,"=",result )
+//      index = index + 1;
+//}
+// let fruit:string []= ["Apple","Mango","Banana","Graps","Gava","Dates","Melon","WaterMelon",]
+// // fruit [5] = "Pineapple"
+// // fruit.pop ()
+// // fruit.unshift("beri")
+// // fruit.shift()
+// fruit.splice(1,1,"pineapple")
+// fruit.splice(2,0,"Fruitchat")
+// console.log(fruit);
+// var rollNumber:number[] =[10,20,30,50,100,500,]
+// rollNumber[1]= 9
+//  console.log(rollNumber[1]);
+// var studenNames:string[] = ["ali","zain","bilal","naveed",""]
+// studenNames[0]="farhan";
+// console.log(studenNames[0]);
+// var index:string[] = ["apple","mango","graps"]
+// if (index.length<=2) 
+// { console.log("lenght is 2");
+// }
+// else 
+//  { (index.length !==2)
+// console.log("lenght in not 2");
+// }
+//// Create a function that takes an array, an index, and a value as parameters. Inside the function, use the splice method to insert the value at the specified index in the array. Return the modified array
+// function withSplic(anArray:string[],anIndex:number,aValue:string) {
+//     anArray.splice(anIndex,0,aValue);
+// return anArray
+// }
+// console.log(withSplic(['Alizar','a','program'],1,'writes'));
+////Implement a simple shopping cart program using an array. Create functions to add items, remove items, and update quantities using the splice method. Print the cart's contents after each operation
+// let shoppingCart:string[]= ["sugar","water","milk","cup"]
+// function addItems(itemsindex:number,itemName:string) {
+//     shoppingCart.splice (1,0,"sugar+","water+")
+//     return shoppingCart
+// }
+// console.log(addItems(0,"cream"));
+// let rollNumber:number [] =[20,25,30,40,45,50,]
+// rollNumber.push(60)
+// console.log('after push',rollNumber);
+//  rollNumber.pop()
+//  console.log('After Pop',rollNumber);
+//  rollNumber.unshift(10,15)
+//  console.log("after Unshift",rollNumber);
+//  rollNumber.shift()
+// console.log("after shift",rollNumber);
+// let names:string[]= ["ali","mohsin","bilal","akbar","akmal","atif","kashif","abbas","raza"]
+// // console.log(names);
+// // names.splice(4,1,"akram","ashraf")
+// // console.log(names);
+// console.log(names.length);
+// names.splice(names.length-5,1,"hassan","ahmad")
+//  console.log(names);
+//  let shopppingCart:string[]= ["potato","tomato","onion","rice","fruit","bread"]
+// //  console.log(shopppingCart.length);
+// //  ////Add Chicken and Mutton
+// //  shopppingCart.splice(3,1,"Chicken","Mutton")
+// //  console.log(shopppingCart);
+// //  //// remove potato
+//  shopppingCart.splice(0,0,"Water")
+//  console.log(shopppingCart);
+//  let n:number = 0;
+//  while (n <= 25) {
+//     console.log(n);
+//     n++;
+//}
+let num = 0;
+while (num <= 10) {
+    console.log(num);
+    num += 3;
+}
