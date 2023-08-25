dfec0df029062c307965c78df2e6f80cb2be066f
Alizarbukhari <Alizar.fsd@gmail.com>
2023-08-25 22:10:27 +0500

    Assignment 06

--- Assignment 04/index.ts
+++ Assignment 04/index.ts
@@ -51,3 +51,39 @@
 
 //     }
 // } 
+//Write a program having an array of numbers if the number is negative it should remove the negative number from the array.
+let indexArray:number[]=[-3,-1,-1,1,-2,3,4,5,]
+console.log(`all arrays number ${indexArray}`);
+for (let i:number = 0; i<indexArray.length;i++) {
+    if (indexArray[i]<0) {
+        indexArray.splice(i,1)
+  
+   
+    }
+    
+}
+ console.log(indexArray);
+
+ ////Create a function that takes an array of numbers as parameter. Use a while loop to calculate and return the sum of all the numbers in the array.
+let index= [5,10,15,20,25,30,];
+function sum(num:number[]){
+    let i=0;
+    let sum = 0;
+    while (i<index.length) {
+        sum=sum+index[i];   
+        i++
+    }
+    return sum;
+}
+//// Implement a program that takes a list of temperatures in Celsius as input from the user. Convert each temperature to Fahrenheit using the formula F = (C * 9/5) + 32 and store the converted temperatures in an array. Use a while loop to perform the conversion for each temperature.
+let temperature = [20,30,40,50,60];
+var fahrenhiet:number []= [];
+function InFahreniet(temperature:number[],fahrenhiet:number[]) {
+let i = 0;
+while (i<temperature.length) {
+    fahrenhiet[i]=(temperature[i]* 9/5)+32;
+    i++
+}    
+}
+InFahreniet(temperature,fahrenhiet)
+console.log(`Temprature list After Conversion ${fahrenhiet}`);
